a4d99a6590c5294be88582421d5b2675f0e3ed93
Samuel Bautista <samuelbaubas@gmail.com>
2022-01-27 16:28:07 -0600

    Usamos un objeto Model para poder agregarle atributos en nuestro contoller y poder mostrarlo en las vistas, en el html del home agregamos una nueva forma de mostrar los atributos agregados y configuramos el workspace para usar thymeleaf

--- src/main/java/net/sam_industries/empleos/controller/HomeController.java
+++ src/main/java/net/sam_industries/empleos/controller/HomeController.java
@@ -1,13 +1,18 @@
 package net.sam_industries.empleos.controller;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String showHome(){
+    public String showHome(Model model){
+        model.addAttribute("mensaje", "Bienvenidos a Empleaos Apps");
+        model.addAttribute("fecha", new Date());
         return "home";
     }
     
